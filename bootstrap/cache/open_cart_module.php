4af95c48be4624c3c0b311afd0a4859524dae5ca
Echelebo <echelebochibuike@gmail.com>
2024-07-08 19:13:23 +0100

    Second Commit

---
+++ bootstrap/cache/open_cart_module.php
@@ -0,0 +1,13 @@
+<?php return array (
+  'providers' => 
+  array (
+    0 => 'Modules\\OpenCart\\Providers\\OpenCartServiceProvider',
+  ),
+  'eager' => 
+  array (
+    0 => 'Modules\\OpenCart\\Providers\\OpenCartServiceProvider',
+  ),
+  'deferred' => 
+  array (
+  ),
+);
